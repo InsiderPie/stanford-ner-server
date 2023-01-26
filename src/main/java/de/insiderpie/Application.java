8b9949190cb257c77b9eaa53b518aea5c2855143
jfhr <me@jfhr.de>
2023-01-26 23:59:29 +0100

    remove unbefitting "Executable" in Application.java
    honestly idk how that got in there.

--- src/main/java/de/insiderpie/Application.java
+++ src/main/java/de/insiderpie/Application.java
@@ -2,14 +2,11 @@ package de.insiderpie;
 
 import io.micronaut.runtime.Micronaut;
 
-import java.lang.reflect.Executable;
-
 public class Application {
 
     public static void main(String[] args) {
         System.out.println("Stanford NER server, made with <3 by InsiderPie");
         Micronaut.run(Application.class, args);
-        Executable
     }
 }
 
